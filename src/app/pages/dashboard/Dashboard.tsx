69099aeee8733045c54b15f9b22f44a03bfc54df
Anderson Tiago <andersonbonamim@gmail.com>
2022-07-04 19:56:27 -0300

    Criando servidor com JSON server, criando o CRUD

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,70 @@
-import { Link } from 'react-router-dom'
+import { useCallback, useState } from "react"
+
+interface ITarefa {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+}
 
 export const Dashboard = () => {
+    const [lista, setLista] = useState<ITarefa[]>([])
+
+    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if (e.key === 'Enter') {
+            if (e.currentTarget.value.trim().length === 0) return;
+
+            const value = e.currentTarget.value
+
+            e.currentTarget.value = ''
+
+            setLista((oldList) => {
+
+                if (oldList.some((listItem) => listItem.title === value)) return oldList
+
+                return [...oldList, {
+                    id: oldList.length,
+                    title: value,
+                    isCompleted: false
+                }]
+            })
+        }
+    }, [])
+
     return (
         <div>
-            <p>Dashboard</p>
-            <Link to="/autenticar">Login</Link>
+            <p>Lista</p>
+
+            <input
+                onKeyDown={handleInputKeyDown}
+            />
+
+            <p>{lista.filter((listItem) => listItem.isCompleted).length}</p>
+
+            <ul>
+                {
+                    lista.map((listItem) => {
+                        return <li key={listItem.id}>
+                            <input type="checkbox"
+                                checked={listItem.isCompleted}
+                                onChange={() => {
+                                    setLista(oldList => {
+                                        return oldList.map(oldListItem => {
+                                            const newisCompleted = oldListItem.title === listItem.title
+                                                ? !oldListItem.isCompleted
+                                                : oldListItem.isCompleted
+                                            return {
+                                                ...oldListItem,
+                                                isCompleted: newisCompleted,
+                                            }
+                                        })
+                                    })
+                                }}
+                            />
+                            {listItem.title}
+                        </li>
+                    })
+                }
+            </ul>
         </div>
     )
 }
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,28 +1,54 @@
-import { useState } from "react"
+import { useCallback, useMemo, useRef, useState } from "react"
+import { useUsuarioLogado } from "../../shared/hooks"
+import { ButtonLogin } from "./components/ButtonLogin"
+import { InputLogin } from "./components/InputLogin"
 
 export const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setpassword] = useState('')
+    const inputPasswordRef = useRef<HTMLInputElement>(null)
 
-    const handleEntrar = () => {
+    const { nomeDoUsuario } = useUsuarioLogado()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const emailLength = useMemo(() => {
+        return email.length * 1000
+    }, [email.length])
+
+    const handleEntrar = useCallback(() => {
         console.log(email, password)
-    }
+
+        if (inputPasswordRef.current !== null) {
+            inputPasswordRef.current.focus()
+        }
+
+    }, [email, password])
 
     return (
         <div>
             <form>
-                <label>
-                    <span>Email</span>
-                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-                </label>
-                <label>
-                    <span>Senha</span>
-                    <input type="password" value={password} onChange={e => setpassword(e.target.value)} />
-                </label>
+                <p>Quantidade de caracteres no email: {emailLength} </p>
+                <p>{nomeDoUsuario}</p>
+                <InputLogin
+                    label="Email"
+                    type="text"
+                    value={email}
+                    onChange={newValue => setEmail(newValue)}
+                    onPressEnter={() => inputPasswordRef.current?.focus()}
+                />
 
-                <button type="button" onClick={handleEntrar}>
-                    Entrar
-                </button>
+                <InputLogin
+                    label="Senha"
+                    type="password"
+                    value={password}
+                    ref={inputPasswordRef}
+                    onChange={newValue => setPassword(newValue)}
+                />
+
+                <ButtonLogin
+                    type="button"
+                    onClick={handleEntrar}
+                >Entrar</ButtonLogin>
             </form>
         </div>
     )
