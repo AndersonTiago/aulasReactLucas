e27a19651734e82760ebde8cf7b0f84403d0e18f
Anderson Tiago <andersonbonamim@gmail.com>
2023-01-18 20:19:40 -0300

    testando Wrkers

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 
+const workerPath = "./worker.js";
+
+const { serviceWorker } = window.navigator;
+
+
+serviceWorker.register(workerPath, {})
+  .then(result => {
+
+    const worker = result.active;
+
+    worker?.postMessage('{"name": "worker"}');
+
+    // Uma requisição aleatória para demonstração do cache do Service Worker
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+  })
+  .catch(error => console.log(error));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
